9f949d95be5b42129bbe4cc332eeadbd7f47bcb0
Martin Hujer <mhujer@gmail.com>
2019-08-27 15:30:05 +0200

    bump phpunit/phpunit to 8.3

--- Test/BundleTest.php
+++ Test/BundleTest.php
@@ -1,9 +1,4 @@
 <?php
-// The condition is needed for testing the bundle against Symfony versions 2.3.* and 3.0.*
-if (!class_exists('\PHPUnit_Framework_TestCase') &&
-    class_exists('\PHPUnit\Framework\TestCase')) {
-    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
-}
 
 class BundleTest extends \Symfony\Bundle\FrameworkBundle\Test\WebTestCase
 {
@@ -14,10 +9,10 @@ class BundleTest extends \Symfony\Bundle\FrameworkBundle\Test\WebTestCase
         $container = $client->getContainer();
 
         // Test if the service exists
-        $this->assertTrue($container->has('white_october_breadcrumbs.helper'));
+        self::assertTrue($container->has('white_october_breadcrumbs.helper'));
 
         $service = $container->get('white_october_breadcrumbs.helper');
-        $this->assertInstanceOf(\WhiteOctober\BreadcrumbsBundle\Templating\Helper\BreadcrumbsHelper::class, $service);
+        self::assertInstanceOf(\WhiteOctober\BreadcrumbsBundle\Templating\Helper\BreadcrumbsHelper::class, $service);
     }
 
     public function testRendering()
